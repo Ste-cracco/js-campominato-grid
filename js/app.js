3ac830424b9c5808c2ea2fcfc264d5594bde0cf2
Ste-cracco <s.cracco@yahoo.it>
2022-07-29 14:37:05 +0200

    Inserito numeri in ogni cella della griglia

--- js/app.js
+++ js/app.js
@@ -3,8 +3,9 @@ const buttonElement = document.querySelector('.bottone');
 
 buttonElement.addEventListener('click', function() {
 
-    for(let i = 0; i < 100; i++) {
+    for(let i = 1; i <= 100; i++) {
         const divCella = document.createElement('div');
+        divCella.append(i);
         divCella.classList.add('cella');
         grigliaElement.append(divCella);
     }
