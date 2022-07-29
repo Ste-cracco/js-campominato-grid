c44c97aeb8dc83bbd09b6005ba8233da54adbefe
Ste-cracco <s.cracco@yahoo.it>
2022-07-29 14:53:42 +0200

    Stessa cosa, ma creato tutto con le funzioni

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 const grigliaElement = document.querySelector('.container_griglia');
 const buttonElement = document.querySelector('.bottone');
 
+// Funzioni:
+
+function creazioneCella() {
+    const divCella = document.createElement('div');
+    divCella.classList.add('cella');
+    return divCella;
+}
+
+function gestoreClick() {
+    console.log('click')
+}
+
+
 buttonElement.addEventListener('click', function() {
 
-    for(let i = 1; i <= 100; i++) {
-        const divCella = document.createElement('div');
-        divCella.append(i);
-        divCella.classList.add('cella');
-        grigliaElement.append(divCella);
+    for(let i = 0; i < 100; i++) {
+        
+        const cella = creazioneCella();
+        cella.innerHTML = i + 1;
+        grigliaElement.append(cella);        
     }
 });
+
