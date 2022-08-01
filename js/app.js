6e167b01d05ec49b43305ec44cf7dd55ce5feaaf
Ste-cracco <s.cracco@yahoo.it>
2022-08-01 14:27:18 +0200

    probelma array caselle bomba

--- js/app.js
+++ js/app.js
@@ -1,33 +1,52 @@
-
-// Funzioni:
-function creazioneCella(i) {
-    const divCella = document.createElement('div');
-    divCella.classList.add('cella');
-    divCella.addEventListener('click', function() {
-        gestoreClick(i+1)
-        divCella.classList.add('active');
-    });
-    return divCella;
-}
-
-function gestoreClick(i) {
-    console.log(i);
-
-}
-// Fine Funzioni
-
-
 const grigliaElement = document.querySelector('.container_griglia');
-const buttonElement = document.querySelector('.bottone');
+const buttonElement = document.querySelector('.bottone_play');
+buttonElement.addEventListener('click', creaGriglia)
 
-buttonElement.addEventListener('click', function() {
+let posizioniBombe = []
 
+function creaGriglia() {
+    resetGame()
+
+    posizioniBombe = creaBomba()
+    
+    console.log(posizioniBombe)
+    
     for(let i = 0; i < 100; i++) {        
-        const cella = creazioneCella(i);
+        const cella = creaCella();
         cella.innerHTML = i + 1;
         grigliaElement.append(cella);                
     }
-});
+}
 
+function resetGame () {
+    grigliaElement.innerHTML = '';
+}
 
+function creaCella() {
+    const divCella = document.createElement('div');
+    divCella.classList.add('cella');
+    divCella.addEventListener('click', gestoreClick)
+    return divCella;
+}
 
+function gestoreClick() {
+    const cella = this
+    cella.classList.add('active')
+    console.log(cella.innerHTML)
+    cella.removeEventListener('click', gestoreClick)
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+function creaBomba() {
+    const bombe = []
+    while(bombe.length < 16) {
+        const n = getRandomIntInclusive(1, 100)
+        bombe.push(n)
+    }  
+    return bombe    
+}
